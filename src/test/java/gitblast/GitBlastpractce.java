7d0d1555b5bbece505b0cff2a1f5e06efec2f5c7
Praveen85994 <130534267+Praveen85994@users.noreply.github.com>
2023-08-09 15:06:22 +0530

    Update GitBlastpractce.java

--- src/test/java/gitblast/GitBlastpractce.java
+++ src/test/java/gitblast/GitBlastpractce.java
@@ -3,6 +3,10 @@ package gitblast;
 public class GitBlastpractce {
 	public static void main(String[] args) {
 		System.out.println("bashhh");
+
+
+		//do some changes in github and click on save pull...
+		//git pull origin branch name
 	}
 
 }
